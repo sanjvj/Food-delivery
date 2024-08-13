359bf5cd73cebf0c433799608010dad8e05e144e
sanjvj <sanjaysurya3010@gmail.com>
2024-08-13 07:07:32 +0530

    Made progress

--- frontend/src/Components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/Components/ExploreMenu/ExploreMenu.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import './ExploreMenu.css'
 import { menu_list } from '../../assets/food del assets/frontend_assets/assets'
-const ExploreMenu = () => {
+const ExploreMenu = ({category,setCategory}) => {
+  console.log(category);
   return (
     <div className='explore-menu' id='explore-menu'>
       <h1>Explore our Menu</h1>
@@ -9,8 +10,9 @@ const ExploreMenu = () => {
       <div className='explore-menu-list'>
         {menu_list.map((item,index)=>{
             return (
-                <div key={index} className='explore-menu-list-item'>
-                    <img src={item.menu_image}></img>
+                <div onClick={()=>setCategory(prev=>prev===item.menu_name?"All":item.menu_name)} key={index} className='explore-menu-list-item'>
+                    <img className={category===item.menu_name?"active":""} src={item.menu_image}></img>
+                   
                     <p>{item.menu_name}</p>
                 </div>
             )
--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Home.css'
 import Header from '../../Components/Header/Header'
 import ExploreMenu from '../../Components/ExploreMenu/ExploreMenu'
+import FoodDisplay from '../../Components/FoodDisplay/FoodDisplay'
 const Home = () => {
+  const [category,setCategory] = useState("All")
   return (
     <div>
       <Header></Header>
-      <ExploreMenu></ExploreMenu>
+      <ExploreMenu category={category} setCategory={setCategory}></ExploreMenu>
+      <FoodDisplay category={category}></FoodDisplay>
     </div>
   )
 }
